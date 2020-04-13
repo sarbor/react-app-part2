143f7e4048b655b68e66c315ea7a2cc7685236e4
sarbor <sarboroy99@gmail.com>
2020-04-12 20:56:14 -0700

    Added initial form functionality

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form';
+
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]
+    characters: []
   }
 
   render() {
     const { characters } = this.state
     return (
       <div className="container">
-        <Table characterData = {characters} removeCharacter={this.removeCharacter} />
-        
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form />
       </div>
     )
   }
