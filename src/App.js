0e9c3e8f1224f95e12fdd074c395fe2f9dc4de80
sarbor <sarboroy99@gmail.com>
2020-04-15 21:10:18 -0700

    Added post request to backend

--- src/App.js
+++ src/App.js
@@ -10,8 +10,24 @@ class App extends Component {
   }
 
   handleSubmit = character => {
-    this.setState({ characters: [...this.state.characters, character] })
-  }
+     this.makePostCall(character).then( callResult => {
+        if (callResult === true) {
+           this.setState({ characters: [...this.state.characters, character] });
+        }
+     });
+   }
+
+  makePostCall(character){
+   return axios.post('http://localhost:5000/users', character)
+    .then(function (response) {
+      console.log(response);
+      return (response.status === 200);
+    })
+    .catch(function (error) {
+      console.log(error);
+      return false;
+    });
+ }
 
   removeCharacter = index => {
     const { characters } = this.state
