9f6b92d88547c5262af5b6b5bfb8279561330b6f
sarbor <sarboroy99@gmail.com>
2020-04-15 21:54:28 -0700

    Returned entire response from post request

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 
   handleSubmit = character => {
      this.makePostCall(character).then( callResult => {
-        if (callResult === true) {
+        if (callResult.status === 201) {
            this.setState({ characters: [...this.state.characters, character] });
         }
      });
@@ -20,8 +20,7 @@ class App extends Component {
   makePostCall(character){
    return axios.post('http://localhost:5000/users', character)
     .then(function (response) {
-      console.log(response);
-      return (response.status === 200);
+      return response;
     })
     .catch(function (error) {
       console.log(error);
